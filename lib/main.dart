4d82b358335e203d58a916ec43a07e2fd77346c3
jobayed003 <jobayed803@gmail.com>
2023-12-29 15:28:59 +0600

    fixed api issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/ui/get_started.dart';
+import 'package:weather_app/ui/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
-      title: 'Weather App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const GetStarted(),
     );
   }
 }
